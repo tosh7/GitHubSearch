013de0acc8aa51ecdd7f3c5c96ce9a9e4fc58a12
tosh7 <zlia.6.lj.425@gmail.com>
2019-05-08 21:17:56 +0900

    Initial Commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Satoshi Komatsu on 2019/05/08.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
