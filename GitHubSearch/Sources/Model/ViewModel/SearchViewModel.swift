25f5ffc213d942e3137cbc7d98112111bd333e6d
tosh7 <zlia.6.lj.425@gmail.com>
2019-06-26 02:16:02 +0900

    made ViewModel

---
+++ GitHubSearch/Sources/Model/ViewModel/SearchViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewModel.swift
+//  GitHubSearch
+//
+//  Created by Satoshi Komatsu on 2019/06/26.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class SearchViewModel {
+    
+}
