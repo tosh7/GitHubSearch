860af8c23874aa07de9b583c96103a529ea18147
tosh7 <zlia.6.lj.425@gmail.com>
2019-06-26 03:05:23 +0900

    少し書き始めた

--- GitHubSearch/Sources/Model/ViewModel/SearchViewModel.swift
+++ GitHubSearch/Sources/Model/ViewModel/SearchViewModel.swift
@@ -10,8 +10,20 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-final class SearchViewModel {
+final class SearchViewModel: NSObject {
     
     let disposeBag = DisposeBag()
     
+}
+
+extension SearchViewModel: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomTableViewCell
+        
+        return cell
+    }
 }
